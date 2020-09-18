d8450d299885a1827bd2e35fb42b34d42ad29419
Kiril Vatev <vatev.1@gmail.com>
2020-09-18 16:14:25 -0400

    adding tabs in order to have views for multiple tools

--- renderer/App/App.js
+++ renderer/App/App.js
@@ -1,15 +1,59 @@
-const { html, css } = require('../tools/ui.js');
+const { AppBar, Tabs, Tab, html, css, useEffect, useState, setVar } = require('../tools/ui.js');
 const { withConfig } = require('../tools/config.js');
 const { withTheme } = require('../tools/theme.js');
 
 const VideoContainer = require('../VideoContainer/VideoContainer.js');
+const VideoX264 = require('../VideoX264/VideoX264.js');
 
 css('./App.css');
 
+const TABS = [
+  'video container',
+  'transcode to x264'
+].reduce((obj, name, idx) => {
+  obj[idx] = name;
+  obj[name] = idx;
+
+  return obj;
+}, {});
+
+const PANELS = {
+  'video container': VideoContainer,
+  'transcode to x264': VideoX264
+};
+
 function App() {
+  const [tab, setTab] = useState('video container');
+  const app = {};
+  const tabBar = {};
+
+  useEffect(() => {
+    const { height } = tabBar.current.getBoundingClientRect();
+    setVar(app.current, 'tabs-height', `${height}px`);
+  }, []);
+
+  const onTabChange = (ev, newValue) => {
+    if (newValue === Number(TABS[tab])) {
+      return;
+    }
+
+    setTab(TABS[newValue]);
+  };
+
+  const tabDom = Object.keys(PANELS).map(name => {
+    return html`<${Tab} label=${name} />`;
+  });
+
   return html`
-    <div class=app>
-      <${VideoContainer} />
+    <div ref=${tabBar}>
+      <${AppBar} position=static>
+        <${Tabs} ref=${tabBar} value=${TABS[tab]} onChange=${onTabChange}>
+          ${tabDom}
+        <//>
+      <//>
+    </div>
+    <div class=app ref=${app}>
+      <${PANELS[tab]} />
     </div>
   `;
 }
---
+++ renderer/VideoX264/VideoX264.js
@@ -0,0 +1,3 @@
+const { html } = require('../tools/ui.js');
+
+module.exports = () => html`TODO transcode to x264`;
