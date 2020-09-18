f2f3a79660c3e3224a4e5398f982ed3e85be4ea0
Kiril Vatev <vatev.1@gmail.com>
2020-09-18 11:45:22 -0400

    making buttons small and bold

--- renderer/tools/ui-components.js
+++ renderer/tools/ui-components.js
@@ -1,13 +1,16 @@
 const components = require('@material-ui/core');
 
+//const { compose, palette, spacing, typography } = require('@material-ui/system');
+//const Box = components.styled('div')(compose(spacing, palette, typography));
+
 module.exports = ({ html }) => {
 
   const PrimaryButton = ({ ...props }) => html`
-    <${components.Button} ...${props} color=primary variant=contained />
+    <${components.Button} size=small ...${props} style=${{ fontWeight: 'bold' }} color=primary variant=contained />
   `;
 
   const SecondaryButton = ({ ...props }) => html`
-    <${components.Button} ...${props} color=primary variant=outlined />
+    <${components.Button} size=small ...${props} color=primary variant=outlined />
   `;
 
   const PrimaryTextField = ({ ...props }) => html`
