5d0ba3375db0d79000cc3379aa4001ab7e26ff59
Kiril Vatev <vatev.1@gmail.com>
2020-09-18 17:40:32 -0400

    implementing x264 transcoding

--- renderer/VideoX264/VideoX264.js
+++ renderer/VideoX264/VideoX264.js
@@ -1,3 +1,75 @@
-const { html } = require('../tools/ui.js');
+const path = require('path');
 
-module.exports = () => html`TODO transcode to x264`;
+const { FormControlLabel, Switch, html, css, useState } = require('../tools/ui.js');
+const toast = require('../tools/toast.js');
+const videoTools = require('../../lib/video-tools.js');
+
+const FileInput = require('../FileInput/FileInput.js');
+const NamingFields = require('../NamingFields/NamingFields.js');
+
+css('./VideoX264.css');
+
+function VideoX264() {
+  const [prefix, setPrefix] = useState('');
+  const [suffix, setSuffix] = useState('');
+  const [format, setFormat] = useState('mp4');
+  const [audio, setAudio] = useState(true);
+  const [video, setVideo] = useState(true);
+
+  const onQueue = (files) => {
+    for (let file of files) {
+      if (!/^video/.test(file.type || '')) {
+        toast.error(`cannot convert "${file.name}" of type "${file.type}"`);
+        continue;
+      }
+
+      const _suffix = suffix ? suffix :
+        path.parse(file.path).ext === `.${format}` ? '.repack' : '';
+
+      videoTools.queue('x264', [{
+        input: file.path,
+        video,
+        audio,
+        format,
+        prefix,
+        suffix: _suffix
+      }]).then(() => {
+        toast.success(`"${file.name}" is complete`);
+      }).catch(err => {
+        toast.error(`"${file.name}" failed:\n${err.message}`);
+      });
+    }
+  };
+
+  const controlsDom = html`
+    <h3>Transcode</h3>
+    <p><i>Channels that are not transcoded will be copied directory.</i></p>
+    <div>
+      <${FormControlLabel}
+        control=${html`<${Switch} checked=${audio} onChange=${(e, v) => setAudio(v)} />`}
+        label="Transcode Audio"
+      />
+      <${FormControlLabel}
+        control=${html`<${Switch} checked=${video} onChange=${(e, v) => setVideo(v)} />`}
+        label="Transcode Video"
+      />
+    </div>
+  `;
+
+  const namingDom = html`<${NamingFields} nooutput ...${{
+    prefix, setPrefix,
+    suffix, setSuffix,
+    format, setFormat
+  }}/>`;
+
+  return html`
+    <div class=video-x264>
+      <p>Drag files here to encode to x264</p>
+      <${FileInput} nobutton onchange=${onQueue} />
+      ${controlsDom}
+      ${namingDom}
+    </div>
+  `;
+}
+
+module.exports = VideoX264;
