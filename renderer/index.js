2663ccc9ac8c75ed3773680f11495aba061ee498
Kiril Vatev <vatev.1@gmail.com>
2020-09-18 11:58:04 -0400

    adding fonts that can be used offline

--- renderer/index.js
+++ renderer/index.js
@@ -4,5 +4,6 @@ const { render, html, css } = require('./tools/ui.js');
 const App = require('./App/App.js');
 
 css('./base.css');
+css('../fonts/fonts.css');
 
 render(html`<${App} />`, document.querySelector('#app'));
