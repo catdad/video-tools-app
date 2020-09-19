c5674aa1f3aa9d7cdd93c9ada8be0e7ab7a14eb1
Kiril Vatev <vatev.1@gmail.com>
2020-09-19 19:26:20 -0400

    adding tests matching the app with the new tab system

--- test/smoke.test.js
+++ test/smoke.test.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-const waitForThrowable = require('wait-for-throwable');
+const { getDocument, queries: { findByText } } = require('pptr-testing-library');
 
 const { start, stop } = require('./lib/app-provider.js');
 const config = require('./lib/config-provider.js');
@@ -34,12 +34,46 @@ describe('[smoke tests]', () => {
     const app = await start(configPath);
 
     await app.utils.waitForVisible('#app');
+  });
 
-    await waitForThrowable(async () => {
-      const buttons = await app.page.$$('p');
-      expect(buttons).to.have.length.above(0);
+  it('is on the container tab by default', async () => {
+    const configPath = await config.create({});
+    const app = await start(configPath);
 
-      expect(await app.utils.getText('p')).to.equal('Drag files here to change the video container');
-    });
+    await app.utils.waitForVisible('#app');
+
+    const $document = await getDocument(app.page);
+
+    await findByText($document, 'video container');
+    const title = await findByText($document, 'Drag files here to change the video container');
+
+    expect(await title.evaluate(e => e.tagName)).to.equal('H2');
+  });
+
+  it('can switch to the x264 tab', async () => {
+    const configPath = await config.create({});
+    const app = await start(configPath);
+
+    await app.utils.waitForVisible('#app');
+
+    const $document = await getDocument(app.page);
+
+    const tab = await findByText($document, 'transcode to x264');
+    await tab.click();
+
+    const title = await findByText($document, 'Drag files here to encode to x264');
+    expect(await title.evaluate(e => e.tagName)).to.equal('H2');
+  });
+
+  it('opens the defined tab if one is present in config', async () => {
+    const configPath = await config.create({ 'default-tab': 'transcode to x264' });
+    const app = await start(configPath);
+
+    await app.utils.waitForVisible('#app');
+
+    const $document = await getDocument(app.page);
+
+    const title = await findByText($document, 'Drag files here to encode to x264');
+    expect(await title.evaluate(e => e.tagName)).to.equal('H2');
   });
 });
