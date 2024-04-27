d32f96e5f46014520ce6db5123bf71c5b186685a
Kiril Vatev <vatev.1@gmail.com>
2024-04-26 23:58:33 -0400

    using icons

--- renderer/Frame/Frame.js
+++ renderer/Frame/Frame.js
@@ -6,15 +6,19 @@ const {
 } = require('../tools/ui.js');
 const browser = require('../../lib/browser.js');
 
+const Close = require('@material-ui/icons/Close').default;
+const Minimize = require('@material-ui/icons/Minimize').default;
+const Maximize = require('@material-ui/icons/WebAsset').default;
+
 css('./Frame.css');
 
 const Frame = () => {
   return html`<div class="frame" style="height: 30px">
     <div class="frame-title">Video Tools</div>
     <div class="frame-buttons">
-      <button onClick=${() => browser.minimize()}>Min</button>
-      <button onClick=${() => browser.maximizeToggle()}>Max</button>
-      <button onClick=${() => browser.close()}>Close</button>
+      <button onClick=${() => browser.minimize()}><${Minimize} /></button>
+      <button onClick=${() => browser.maximizeToggle()}><${Maximize} /></button>
+      <button onClick=${() => browser.close()}><${Close} /></button>
     </div>
   </div>`;
 };
