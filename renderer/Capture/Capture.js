e56002ad3620f35f2181d80e938f05b838978fb0
Kiril Vatev <vatev.1@gmail.com>
2024-04-28 17:26:02 -0400

    implementing basic ui

--- renderer/Capture/Capture.js
+++ renderer/Capture/Capture.js
@@ -1,7 +1,24 @@
-const { html } = require('../tools/ui.js');
+const path = require('path');
+
+const { html, Material: M } = require('../tools/ui.js');
+const { useConfigSignal, useConfigPaths } = require('../tools/config.js');
+
+const videoTools = require('../../lib/video-tools.js');
 
 function Capture() {
-  return html`Nothing to see here... yet`;
+  const { desktop } = useConfigPaths();
+  const outputDirectory = useConfigSignal('capture.output', desktop);
+
+  const onStart = () => {
+    console.log('start recording');
+  };
+
+  return html`
+    <div class="tab-panel capture">
+      <h2>Screen recording</h2>
+      <${M`Button`} onClick=${onStart}>start<//>
+    </div>
+  `;
 }
 
 module.exports = Capture;
