7e16775d75c34a3a0118e2afdd5aa4b33199df04
Kiril Vatev <vatev.1@gmail.com>
2024-04-29 01:50:36 -0400

    allowing enter and exit of transparent mode, adding recording controls to the frame

--- renderer/Capture/Capture.js
+++ renderer/Capture/Capture.js
@@ -1,38 +1,62 @@
 const path = require('path');
 
-const { html, css, Material: M } = require('../tools/ui.js');
+const { html, css, Material: M, batch } = require('../tools/ui.js');
 const { useConfigSignal, useConfigPaths } = require('../tools/config.js');
 
 const videoTools = require('../../lib/video-tools.js');
+const { useTransparent } = require('../tools/transparent.js');
+const { useFrame } = require('../Frame/Frame.js');
 
 css('./Capture.css');
 
 const makeEven = val => val % 2 === 0 ? val : val - 1;
 
 function Capture() {
+  const { isTransparent } = useTransparent();
+  const { frameButtons } = useFrame();
   const { desktop } = useConfigPaths();
   const outputDirectory = useConfigSignal('capture.output', desktop);
 
-  const onStart = () => {
+  const onSetup = () => {
     const x = window.screenX < 0 ? 0 : window.screenX;
     const y = window.screenY < 0 ? 0 : window.screenY;
     const width = makeEven(window.screenX < 0 ? window.outerWidth + window.screenX : window.outerWidth);
     const height = makeEven(window.screenY < 0 ? window.outerHeight + window.screenY : window.outerHeight);
 
-    videoTools.exec('desktop', [{
-      x, y, width, height,
-      offsetX: x,
-      offsetY: y,
-      output: path.resolve(outputDirectory.value, `Screen Recording - ${new Date().toISOString().replace(/:/g, '-')}.mp4`)
-    }])
-      .then(() => console.log('done!'))
-      .catch(err => console.log('failed:', err));
+    const exit = () => {
+      batch(() => {
+        isTransparent.value = false;
+        frameButtons.value = null;
+      });
+    };
+
+    const buttons = html`
+      <button onClick=${() => {
+        videoTools.exec('desktop', [{
+          x, y, width, height,
+          offsetX: x,
+          offsetY: y,
+          output: path.resolve(outputDirectory.value, `Screen Recording - ${new Date().toISOString().replace(/:/g, '-')}.mp4`)
+        }])
+          .then(() => console.log('done!'))
+          .catch(err => console.log('failed:', err))
+          .finally(() => exit());
+      }}>Start</button>
+      <button onClick=${() => {
+        exit();
+      }}>Stop</button>
+    `;
+
+    batch(() => {
+      isTransparent.value = true;
+      frameButtons.value = buttons;
+    });
   };
 
   return html`
     <div class="tab-panel capture">
       <h2>Screen recording</h2>
-      <${M`Button`} onClick=${onStart}>start<//>
+      <${M`Button`} onClick=${onSetup}>Setup<//>
     </div>
   `;
 }
